a760f67f5e79bf1c64432e639c3417b993dfd1e0
Salla S <47078225+sallasal@users.noreply.github.com>
2020-07-28 14:43:10 +0300

    Translate tapahtuma functions and routes

--- projects.py
+++ projects.py
@@ -2,24 +2,24 @@ from db import db
 import users
 
 # Tapahtumia koskevat SQL:t
-def laske_tapahtumat():
+def count_projects():
     result = db.session.execute("SELECT COUNT(*) FROM tapahtumat")
     counter = result.fetchone()[0]
     return counter
 
-def listaa_tapahtumat():
+def list_projects():
     result = db.session.execute("SELECT id, nimi FROM tapahtumat ORDER BY id DESC")
     tapahtumalista = result.fetchall()
     return tapahtumalista
 
-def lisaa_tapahtuma(nimi):
+def add_project(nimi):
     user_id = users.user_id()
     sql = "INSERT INTO tapahtumat (nimi, kayttaja) VALUES (:nimi,:user_id)"
     db.session.execute(sql, {"nimi":nimi,"user_id":user_id})
     db.session.commit()
     return True
 
-def hae_tapahtuma(id):
+def get_project(id):
     sql = "SELECT id, nimi, kayttaja FROM tapahtumat WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     tapahtumatiedot = result.fetchone()
--- routes.py
+++ routes.py
@@ -1,31 +1,31 @@
 from app import app
-import osaprojektit, tapahtumat, users
+import osaprojektit, projects, users
 from flask import render_template, redirect, session, request
 
 # Sivujen reititykset
 @app.route("/")
 def index():
-    laskuri = tapahtumat.laske_tapahtumat()
-    tapahtumalista = tapahtumat.listaa_tapahtumat()
-    return render_template("index.html", laskuri = laskuri, tapahtumalista = tapahtumalista)
+    counter = projects.count_projects()
+    project_list = projects.list_projects()
+    return render_template("index.html", counter = counter, project_list = project_list)
 
-@app.route("/createtapahtuma", methods=["GET","POST"])
-def createtapahtuma():
+@app.route("/createproject", methods=["GET","POST"])
+def createproject():
     if request.method == "GET":
-        return render_template("createtapahtuma.html")
+        return render_template("createproject.html")
     if request.method == "POST":
         nimi = request.form["nimi"]
-        if tapahtumat.lisaa_tapahtuma(nimi):
+        if projects.add_project(nimi):
             return redirect("/")
         else:
             return render_template("error.html",message="Tapahtuman luominen ei jostain syystä onnistunut. Tarkista arvo ja yritä uudelleen.")
 
-@app.route("/tapahtuma/<int:id>", methods=["GET"])
-def tapahtuma(id):
-    tapahtumatiedot = tapahtumat.hae_tapahtuma(id)
+@app.route("/project/<int:id>", methods=["GET"])
+def project(id):
+    tapahtumatiedot = projects.get_project(id)
     jarjestaja_id = tapahtumatiedot[2]
     kayttajatiedot = users.user_name(jarjestaja_id)
-    return render_template("tapahtuma.html", tapahtumatiedot = tapahtumatiedot, kayttajatiedot = kayttajatiedot)
+    return render_template("project.html", tapahtumatiedot = tapahtumatiedot, kayttajatiedot = kayttajatiedot)
 
 #Osaprojekteihin liittyvät reititykset
 @app.route("/createosaprojekti", methods=["POST"])
@@ -34,7 +34,7 @@ def createosaprojekti():
     nimi = request.form["nimi"]
     budjettisumma = request.form["budjettisumma"]
     if osaprojektit.lisaa_osaprojekti(nimi,budjettisumma,tapahtumaid):
-        return redirect("/tapahtuma/"+tapahtumaid)
+        return redirect("/project/"+tapahtumaid)
     else:
         return render_template("error.html",message="Osa-alueen lisääminen ei onnistunut. Tarkista arvot ja yritä uudelleen.")
 
